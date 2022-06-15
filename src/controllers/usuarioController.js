25654fc37be2dfde3f8fb3eedf7cbede86dfa990
Ana Clara Custodio <anaccustodio8@gmail.com>
2022-06-15 15:39:39 -0300

    adicionando rota para editar dados do perfil

--- src/controllers/usuarioController.js
+++ src/controllers/usuarioController.js
@@ -1,6 +1,10 @@
 const usuarioController = {
     perfil: (req, res)=>{
         res.render('painel-do-usuario')
+    },
+
+    editar: (req, res)=>{
+        res.render('painel-do-usuario-detalhado')
     }
 }
 
--- src/routes/usuarioRouter.js
+++ src/routes/usuarioRouter.js
@@ -3,5 +3,6 @@ const router = express.Router();
 const usuarioController = require('../controllers/usuarioController')
 
 router.get('/perfil', usuarioController.perfil);
+router.get('/perfil/editar', usuarioController.editar);
 
 module.exports = router;
