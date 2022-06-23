8fe29089d9e05f78702872ae6f474a149abc4cf2
Ana Clara Custodio <anaccustodio8@gmail.com>
2022-06-23 10:53:48 -0300

    adicionando rota e controller da pag de sucesso

--- app.js
+++ app.js
@@ -1,8 +1,10 @@
 const express = require('express');
 const path = require('path');
+const compraController = require('./src/controllers/compraController');
 const porta = 5000
 
 const usuarioRouter = require('./src/routes/usuarioRouter')
+const compraRouter = require('./src/routes/compraRouter')
 
 const app = express();
 
@@ -11,6 +13,7 @@ app.set('view engine', 'ejs');
 app.use(express.json());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use(usuarioRouter)
+app.use(usuarioRouter);
+app.use(compraRouter);
 
 app.listen(porta, () => console.log("Aplicação rodando em http://localhost:5000"));
