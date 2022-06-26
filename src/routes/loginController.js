4f54273f23ec685c1ec095cef95ee99b16928f5f
Edkush42 <mahalbrabo@gmail.com>
2022-06-26 03:57:24 -0300

    View de registro iniciada

--- src/routes/loginController.js
+++ src/routes/loginController.js
@@ -3,5 +3,5 @@ const router = express.Router()
 const loginController = require('../controllers/loginController')
 
 router.get('/login', loginController.login)
-
+router.get('/registre-se' ,loginController.registro)
 module.exports  = router
