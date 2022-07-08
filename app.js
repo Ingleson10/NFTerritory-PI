2cb301be11a227ede60acf259c4bf7ee52c09ae3
Ana Clara Custodio <anaccustodio8@gmail.com>
2022-07-07 21:59:47 -0300

    mexendo na rota/controller/view da pag de cadastro

--- app.js
+++ app.js
@@ -6,7 +6,8 @@ const usuarioRouter = require('./src/routes/usuarioRouter')
 const compraRouter = require('./src/routes/compraRouter')
 const indexRouter = require('./src/routes/indexRouter')
 const loginRouter = require('./src/routes/loginRouter')
-const carrinhoRouter = require('./src/routes/carrinhoRouter')
+const carrinhoRouter = require('./src/routes/carrinhoRouter');
+const cadastroRouter = require('./src/routes/cadastroRouter');
 const app = express();
 
 app.set('views', path.join(__dirname, 'src', 'views'));
@@ -19,4 +20,5 @@ app.use(compraRouter)
 app.use(indexRouter)
 app.use(loginRouter)
 app.use(carrinhoRouter)
+app.use(cadastroRouter)
 app.listen(porta, () => console.log("Aplicação rodando em http://localhost:5000"));
--- src/routes/cadastro.js
+++
@@ -1,7 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const produtoInternaController = require('../controllers/cadastro');
-
-router.get('/cadastrarUsuario', cadastroController.cadastrarUsuario);
-
-module.exports = router;
