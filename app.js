e313b690951d5aa0ffe7ed803dc641114d71a48b
Ana Clara Custodio <anaccustodio8@gmail.com>
2022-08-12 17:32:36 -0300

    instalando cookie-parser e express-session

--- app.js
+++ app.js
@@ -1,5 +1,7 @@
 const express = require('express');
 const path = require('path');
+const cookieParser = require('cookie-parser');
+const sessionParser = require('express-session');
 const porta = 5000
 
 const usuarioRouter = require('./src/routes/usuarioRouter')
@@ -10,13 +12,16 @@ const carrinhoRouter = require('./src/routes/carrinhoRouter');
 const cadastroRouter = require('./src/routes/cadastroRouter');
 const produtoRouter = require('./src/routes/produtoRouter');
 const crudRouter = require('./src/routes/crudRouter')
+const sairRouter = require('./src/routes/sairRouter')
 const app = express();
 
+app.use(sessionParser({ secret: 'nfterritory', resave: false, saveUninitialized: false }));
+app.use(cookieParser());
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
 app.set('views', path.join(__dirname, 'src', 'views'));
 app.set('view engine', 'ejs');
-app.use(express.json());
 app.use(express.static(path.join(__dirname, 'public')));
-app.use(express.urlencoded({ extended: false }));
 
 app.use('/usuario', usuarioRouter);
 app.use('/compra', compraRouter);
@@ -26,5 +31,6 @@ app.use('/carrinho', carrinhoRouter);
 app.use('/cadastrar', cadastroRouter);
 app.use('/produtos', produtoRouter);
 app.use(crudRouter)
+app.use(sairRouter)
 
 app.listen(porta, () => console.log("Aplicação rodando em http://localhost:5000"));
