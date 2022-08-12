b8a295d2c11d7942f9cf1677e53e68ce4c1227e5
Ana Clara Custodio <anaccustodio8@gmail.com>
2022-08-12 18:11:56 -0300

    controller do login (funcional, mas usuario ainda nao consegue se manter logado

--- src/controllers/loginController.js
+++ src/controllers/loginController.js
@@ -1,9 +1,38 @@
+const { Usuario } = require('../database/models')
+const bcrypt = require('bcrypt')
+
 const loginController = {
-    login : (req,res) =>{
+    login: (req, res) => {
         res.render('login')
     },
-    registro : (req,res)=>{
-        res.render('registro')
+    logarUsuario: (req, res) => {
+        const { email, senha, lembrar } = req.body;
+        const lembrarUsuario = !!lembrar;
+
+        Usuario.findOne({
+            where: {
+                email,
+            }
+        }).then((usuario) => {
+            if(!usuario){
+                return res.render('login', { error: 'Usuário não cadastrado'})
+            }
+
+            if(!bcrypt.compareSync(senha, usuario.senha)){
+                return res.render('login', { error: 'Email ou senha incorretos'})
+            }
+
+            req.session.usuarioLogado = usuario
+
+            if(lembrarUsuario){
+                res.cookie('usuario', JSON.stringify({ email: usuario.email, nome: usuario.nome_usuario, admin: usuario.admin}), { maxAge: 90000 })
+                console.log(req.cookies.usuario)
+                //cookie vindo undefined, mas em aplication no navegador os dados do usuario aparecem (?)
+            }
+
+            res.redirect('/usuario/perfil')      
+        }).catch((error) => console.log(error))
     }
 }
+
 module.exports = loginController
