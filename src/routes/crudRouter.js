2f53a6f35ef258da243628b30d675a52eed9fe56
Ana Clara Custodio <anaccustodio8@gmail.com>
2022-08-16 09:46:41 -0300

    adicionando middlewares em rotas

--- src/routes/crudRouter.js
+++ src/routes/crudRouter.js
@@ -4,6 +4,7 @@ const crudController = require('../controllers/crudController')
 const multer = require('multer')
 const path = require('path')
 const verificarSeUsuarioEhAdmin = require('../middlewares/verificarSeUsuarioEhAdmin')
+const verificarSeUsuarioEstaLogado = require('../middlewares/verificarSeUsuarioEstaLogado')
 
 const multerDiskStorage = multer.diskStorage({
     destination: (req,file, callback) => {
@@ -11,12 +12,12 @@ const multerDiskStorage = multer.diskStorage({
     callback(null, folder)
     },
     filename: (req,file,callback)=>{
-        const imgName = Date.now() + file.originalname
+        const imgName = Date.now() + '-' + file.originalname
         callback(null, imgName)}
     })
     const upload = multer({storage:multerDiskStorage})
     
-    router.get('/crud', verificarSeUsuarioEhAdmin, crudController.index)
-    router.post('/cadastro', verificarSeUsuarioEhAdmin, upload.single('produto-img'), crudController.cadastro)
+    router.get('/crud', verificarSeUsuarioEstaLogado, verificarSeUsuarioEhAdmin, crudController.index)
+    router.post('/cadastro', verificarSeUsuarioEstaLogado, verificarSeUsuarioEhAdmin, upload.single('produto-img'), crudController.cadastro)
 
 module.exports = router
--- src/routes/usuarioRouter.js
+++ src/routes/usuarioRouter.js
@@ -1,8 +1,9 @@
 const express = require('express');
 const router = express.Router();
 const usuarioController = require('../controllers/usuarioController');
+const verificarSeUsuarioEstaLogado = require('../middlewares/verificarSeUsuarioEstaLogado')
 
-router.get('/perfil', usuarioController.perfil);
-router.get('/perfil/editar', usuarioController.editar);
+router.get('/perfil', verificarSeUsuarioEstaLogado, usuarioController.perfil);
+router.get('/perfil/editar', verificarSeUsuarioEstaLogado,  usuarioController.editar);
 
 module.exports = router;
