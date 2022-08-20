da96925ea41f27a0cbec76af172df6b540b83077
Ana Clara Custodio <anaccustodio8@gmail.com>
2022-08-20 17:56:35 -0300

    crud controller funcional (sem verificações)

--- src/controllers/crudController.js
+++ src/controllers/crudController.js
@@ -1,10 +1,28 @@
+const { Produto } = require('../database/models');
+
 const crudController = {
-    index: (req,res) => {
+    index: (req, res) => {
         res.render('crud')
     },
-    cadastro: (req,res)=>{
-        if(!req.file) return res.send('Arquivo não Enviado')
-        return res.send('Arquivo Enviado!')
+    cadastro: (req, res) => {
+        const { nome, artista, preco, categoria_id, disponibilidade } = req.body
+        console.log(req.body)
+        const filename = req.file.filename
+        if(!req.file) return res.send('Selecione um arquivo')
+        // const extensaoDoArquivo = filename.split(".")[1].toLowerCase();
+
+        // if (extensaoDoArquivo !== "jpg" && extensaoDoArquivo !== "png") {
+        // return res.render('cadastro', { error: 'Formato do arquivo é inválido' })
+        // }
+
+        Produto.create({
+            nome,
+            artista,
+            preco,
+            categoria_id,
+            disponibilidade,
+            imageUrl: filename
+        }).then(res.render('crud', ))
     }
 }
 
