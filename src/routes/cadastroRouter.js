a823130915f5b00bc9445413b92c3221f3a089c2
Ana Clara Custodio <anaccustodio8@gmail.com>
2022-08-23 13:11:19 -0300

    ajeitando alguns detalhes

--- src/routes/cadastroRouter.js
+++ src/routes/cadastroRouter.js
@@ -1,9 +1,8 @@
 const express = require('express');
 const router = express.Router();
 const cadastroController = require('../controllers/cadastroController');
-const validarCadastro = require('../middlewares/validarCadastro')
 
 router.get('/', cadastroController.cadastrar);
-router.post('/', validarCadastro, cadastroController.cadastrarUsuario)
+router.post('/', cadastroController.cadastrarUsuario)
 
 module.exports = router;
