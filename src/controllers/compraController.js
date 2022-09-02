59a4931c50feec80e4ae57a8ef77497031b49cb7
Edkush42 <mahalbrabo@gmail.com>
2022-09-02 01:17:54 -0300

    deixando a view de finalização dinamica e produto interna dinamica

--- src/controllers/compraController.js
+++ src/controllers/compraController.js
@@ -1,12 +1,17 @@
+const {Produto} = require("../database/models");
+const formatarPreco = require("../utils/formatarPreco");
 const compraController = {
-    sucesso: (req, res)=>{
-        res.render('pagina-de-sucesso')
-    },
-    
-    adicionarCartao: (req, res)=>{
-        res.render('finalizacao-compra')
-    },
-}
+  sucesso: (req, res) => {
+    res.render("pagina-de-sucesso");
+  },
 
+  finalizar: (req, res) => {
+    const { id } = req.params;
+    Produto.findOne({where:{id}})
+    .then((resultado) => {
+        res.render('finalizacao-compra', { produto: resultado, formatarPreco })
+    }).catch(error => console.log(error))
+  },
+};
 
-module.exports = compraController
+module.exports = compraController;
